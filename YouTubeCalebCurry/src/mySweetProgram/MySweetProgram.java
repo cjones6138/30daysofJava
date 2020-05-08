92ef6e7acb6a04ffe487abd8d7f5d26305d7ab74
cjones6138 <65043200+cjones6138@users.noreply.github.com>
2020-05-08 16:39:36 -0400

    Add files via upload

---
+++ YouTubeCalebCurry/src/mySweetProgram/MySweetProgram.java
@@ -0,0 +1,20 @@
+package mySweetProgram;
+
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		//Scanner ln = new Scanner(System.in);
+		//String s = ln.nextLine();
+		
+		int x = 5;
+		
+		System.out.println(x);
+		
+	}
+
+}
